47d6bbb8a3d2f82877588889a804a39a4c58b880
nordinvandijk <nordinvandijk@icloud.com>
2024-02-08 23:17:00 +0100

    update

--- installers/macOS.sh
+++ installers/macOS.sh
@@ -1,4 +1,4 @@
 brew tap Homebrew/bundle
 brew bundle
-gh repo clone nordinvandijk/dotfiles
+gh repo clone nordinvandijk/dotfiles ~/documents/github/dotfiles
 stow -d ~/documents/github/dotfiles -t ~/ .
