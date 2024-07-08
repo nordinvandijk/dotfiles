47e44d74b183212b041772770eca1d64b4e34d5c
Nordin van Dijk <nordinvandijk@icloud.com>
2024-07-08 18:44:14 +0200

    Update Ubuntu.sh

--- installers/Ubuntu.sh
+++ installers/Ubuntu.sh
@@ -1,1 +1,6 @@
+apt-get install build-essential procps curl file git
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew tap Homebrew/bundle
+brew bundle
