c64d73cffba023573609505bc497ce23f2ff5d69
Nordin van Dijk <nordinvandijk@icloud.com>
2024-07-08 19:24:21 +0200

    Update Ubuntu.sh

--- installers/Ubuntu.sh
+++ installers/Ubuntu.sh
@@ -6,7 +6,3 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew tap Homebrew/bundle
 brew bundle
 stow -d ~/Documents/GitHub/dotfiles -t ~/ .
-
-# Install packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-~/.local/share/nvim/site/pack/packer/start/packer.nvim
