130ee448d1e9d399246d0f2f9b78fc8aea8600ea
Nordin van Dijk <nordinvandijk@icloud.com>
2024-07-08 22:57:45 +0200

    Update Ubuntu.sh

--- installers/Ubuntu.sh
+++ installers/Ubuntu.sh
@@ -6,3 +6,8 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew tap Homebrew/bundle
 brew bundle
 stow -d ~/Documents/GitHub/dotfiles -t ~/ .
+
+echo $(which zsh) | sudo tee -a /etc/shells
+sudo chsh -s $(which zsh) $USER
+
+zsh
