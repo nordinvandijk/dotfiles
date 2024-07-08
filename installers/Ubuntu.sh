0a1372bcb3384889f7dda6e050714923bdf9976f
Nordin van Dijk <nordinvandijk@icloud.com>
2024-07-08 22:18:07 +0200

    Update Ubuntu.sh

--- installers/Ubuntu.sh
+++ installers/Ubuntu.sh
@@ -1,7 +1,7 @@
 sudo apt-get install build-essential procps curl file git -y
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+source ~/.bashrc
 
 brew tap Homebrew/bundle
 brew bundle
