be60d7939100cc32188c78003bd13a31b9858830
Nordin van Dijk <nordinvandijk@icloud.com>
2025-01-27 23:01:25 +0100

    fix: remove sudo from ubuntu installer script

--- installers/Ubuntu.sh
+++ installers/Ubuntu.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install build-essential procps curl file git -y
+apt-get update
+apt-get install build-essential procps curl file git -y
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
@@ -8,7 +8,7 @@ brew tap Homebrew/bundle
 brew bundle
 stow -d ~/Documents/GitHub/dotfiles -t ~/ .
 
-echo $(which zsh) | sudo tee -a /etc/shells
-sudo chsh -s $(which zsh) $USER
+echo $(which zsh) | tee -a /etc/shells
+chsh -s $(which zsh) $USER
 
 zsh
