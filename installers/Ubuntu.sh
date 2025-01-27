677b42776131697711484fd9847a673ad96cdfa2
Nordin van Dijk <nordinvandijk@icloud.com>
2025-01-27 23:56:49 +0100

    Update Ubuntu.sh

--- installers/Ubuntu.sh
+++ installers/Ubuntu.sh
@@ -1,14 +1,22 @@
-apt-get update
-apt-get install build-essential procps curl file git -y
+# Install packages to set up homebrew
+sudo apt-get update
+sudo apt-get install build-essential procps curl file -y
+
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+# Install packages from Homebrew
 brew tap Homebrew/bundle
 brew bundle
+
+# Symlink the config files to the home directory
 stow -d . -t ~/ .
 
-echo $(which zsh) | tee -a /etc/shells
+# Set zsh as default shell for the current user
+echo $(which zsh) | sudo tee -a /etc/shells
 chsh -s $(which zsh) $USER
 
+# Start the zsh shell
 zsh
