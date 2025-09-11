bc77a5003deb937cef102a657517f518e92b7828
Nordin van Dijk <nordinvandijk@icloud.com>
2025-09-11 11:45:25 +0200

    nvim update

--- home/.config/nvim/lua/nordinvandijk/plugin_config/dap.lua
+++ home/.config/nvim/lua/nordinvandijk/plugin_config/dap.lua
@@ -12,24 +12,71 @@ return {
 
       require("dapui").setup()
       local mason_registry = require("mason-registry")
-      local install_dir = vim.fn.stdpath("data") .. "/mason"
+      local install_dir = vim.fn.stdpath("data") .. "\\mason"
 
       -- Get the installation path for netcoredbg
       -- local netcoredbg_pkg = mason_registry.get_package("netcoredbg")
       -- local netcoredbg_path = netcoredbg_pkg:get_install_path() .. "/netcoredbg"
 
-      -- Configure the .NET adapter (CoreCLR)
       dap.adapters.coreclr = {
         type = "executable",
-        command = install_dir .. '/packages/netcoredbg/netcoredbg',
+        command = install_dir .. '\\packages\\netcoredbg\\netcoredbg\\netcoredbg',
         args = {"--interpreter=vscode"}
       }
 
       dap.adapters.netcoredbg = {
         type = "executable",
-        command = install_dir .. '/packages/netcoredbg/netcoredbg',
+        command = install_dir .. '\\packages\\netcoredbg\\netcoredbg\\netcoredbg',
         args = {"--interpreter=vscode"}
       }
+
+vim.g.dotnet_build_project = function()
+    local default_path = vim.fn.getcwd() .. '/'
+    if vim.g['dotnet_last_proj_path'] ~= nil then
+        default_path = vim.g['dotnet_last_proj_path']
+    end
+    local path = vim.fn.input('Path to your *proj file', default_path, 'file')
+    vim.g['dotnet_last_proj_path'] = path
+    local cmd = 'dotnet build -c Debug ' .. path .. ''
+    print('')
+    print('Cmd to execute: ' .. cmd)
+    local f = os.execute(cmd)
+    if f == 0 then
+        print('\nBuild: ✔️')
+    else
+        print('\nBuild: ❌ (code: ' .. f .. ')')
+    end
+end
+
+vim.g.dotnet_get_dll_path = function()
+    local request = function()
+        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    end
+
+    if vim.g['dotnet_last_dll_path'] == nil then
+        vim.g['dotnet_last_dll_path'] = request()
+    else
+        if vim.fn.confirm('Do you want to change the path to dll?\n' .. vim.g['dotnet_last_dll_path'], '&yes\n&no', 2) == 1 then
+            vim.g['dotnet_last_dll_path'] = request()
+        end
+    end
+
+    return vim.g['dotnet_last_dll_path']
+end
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+            if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
+                vim.g.dotnet_build_project()
+            end
+            return vim.g.dotnet_get_dll_path()
+        end,
+      },
+    }
+
 
       vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
       vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
