de545e94ea883443b410ae2b9348eb87fdf617e0
nordinvandijk <nordinvandijk@icloud.com>
2025-09-11 17:55:17 +0200

    remove wsl clipboard

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -8,15 +8,3 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.clipboard = "unnamedplus"
-vim.g.clipboard = {
-  name = 'WslClipboard',
-  copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
-  },
-  paste = {
-    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-  cache_enabled = 0,
-}
