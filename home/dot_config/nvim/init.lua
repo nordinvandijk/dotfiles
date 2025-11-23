e8b8866d6497bd3a32cc3d3ea180dca43bc783e8
Nordin van Dijk <nordinvandijk@icloud.com>
2025-11-23 13:01:58 +0100

    relative numbers and no more swapfile

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -4,9 +4,14 @@ require("nordinvandijk.plugins")
 require("nordinvandijk.lsp")
 
 vim.o.termguicolors = true
-vim.cmd("set number") -- Line numbers
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.swapfile = false
+vim.opt.undofile = true
