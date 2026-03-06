8d9c20d3777f057553dcd43b7af37f576402c235
Nordin van Dijk <nordinvandijk@icloud.com>
2026-03-06 10:15:49 +0100

    better oil

--- nix/home/nvim/lua/nordinvandijk/plugin_config/oil.lua
+++ nix/home/nvim/lua/nordinvandijk/plugin_config/oil.lua
@@ -3,15 +3,18 @@ return  {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
-     columns = {
-    -- "icon",
-    -- "permissions",
-    -- "size",
-    -- "mtime",
-  },
+    columns = {
+      "icon",
+      -- "permissions",
+      -- "size",
+      -- "mtime",
+    },
+    keymaps = {
+      ["<C-p>"] = false,
+    },
     view_options = {
-      show_hidden = true
-    }
+      show_hidden = true,
+    },
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
