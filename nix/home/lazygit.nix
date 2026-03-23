67f2fd4b6f6cb6e02a75e8238501c03f3cc317eb
Nordin van Dijk <nordinvandijk@icloud.com>
2026-03-23 09:33:02 +0100

    Lazygit custom command

--- nix/home/lazygit.nix
+++ nix/home/lazygit.nix
@@ -7,6 +7,21 @@
         openLink = "xdg-open {{link}}";
       };
       notARepository = "quit";
+      customCommands = [
+        {
+          key = "W";
+          description = "Add worktree in ../";
+          command = ''git worktree add ../{{index .PromptResponses 0}} {{index .PromptResponses 0}}'';
+          context = "localBranches";
+          prompts = [
+            {
+              type = "input";
+              title = "Branch / folder name (created under ../)";
+            }
+          ];
+          loadingText = "Creating worktree...";
+        }
+      ];
     };
   };
 }
